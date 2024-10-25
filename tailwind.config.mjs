00d6f1d0ab676dcd75fdd3a14a2e8ed0316b6ebc
Sofia Weng <sowe0001@stud.kea.dk>
2024-10-25 20:10:33 +0200

    custom colors

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,13 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [
-        require('@tailwindcss/typography'),
-        require('@tailwindcss/forms'),
-        require('@tailwindcss/aspect-ratio'),
-        require('@tailwindcss/container-queries'),
-    ],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      colors: {
+        customWhite: "#F8F8F8",
+        customBlack: "#090F30",
+        customRed: "#FF2A00",
+        customLightGreen: "#81B47E",
+        customDarkGreen: "#346332",
+      },
+    },
+  },
+  plugins: [
+    require("@tailwindcss/typography"),
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+    require("@tailwindcss/container-queries"),
+  ],
+};
