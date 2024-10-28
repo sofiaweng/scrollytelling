fedf5c4cb1d90581c63b29dc579689bc02ef3f5c
bolettuce <bobi0001@stud.kea.dk>
2024-10-28 18:34:50 +0100

    Hero

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,2 +1,8 @@
 /// <reference path="../.astro/types.d.ts" />
 /// <reference types="astro/client" />
+
+// Declare image modules
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.svg';
